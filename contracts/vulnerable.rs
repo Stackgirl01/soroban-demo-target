c2ef16f6cee18fb56d3f7a4e8b60cb810550c161
Syco Smile <Jamesdavidomeiza@gmail.com>
2026-07-29 23:50:04 +0100

    Update withdraw logic

--- contracts/vulnerable.rs
+++ contracts/vulnerable.rs
@@ -7,7 +7,6 @@ pub struct VulnerableContract;
 #[contractimpl]
 impl VulnerableContract {
     pub fn withdraw(env: Env, from: Address, amount: i128) {
-        from.require_auth();
         let balance: i128 = env.storage().instance().get(&from).unwrap_or(0);
         let new_balance = balance - amount;
         env.storage().instance().set(&from, &new_balance);
